4bd25d6f01a8f221bb5b5e8b161c5f59535ddd04
Annabel Smith <annabel@smithecology.org>
2016-04-12 16:16:06 +1000

    add switch for the second time

--- strcalc.R
+++ strcalc.R
@@ -1,15 +1,16 @@
 
-compute = function (input_string) {
+compute = function(input_string) {
+	# Do simple arithmetic from strings
+	# 
 	values = unlist(strsplit(input_string, ' '))
 	num0 = as.integer(values[1])
 	operator =  values [2]
 	num1 = as.integer(values[3])
-	result <- switch (operator,
-	'+' = num0 + num1,
-	'-' = num0 - num1,
-	NA)
-	if (is.na(result)){
-		print ('Unknown operator!')
-	}
-	return(result)
+	result <- switch(operator,
+                   '+' = num0 + num1,
+                   '-' = num0 - num1,
+                   NA)
+  if (is.na(result)) {
+    print('Unknown operator!')
+  }
 }
